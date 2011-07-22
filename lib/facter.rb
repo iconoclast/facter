77d3427a9db02eeb70672a4a286c16d71ec8f214
michael <michael@iconoclast.net>
2011-07-22 12:35:48 -0700

    Committing changes used to build facter.jar

--- lib/facter.rb
+++ lib/facter.rb
@@ -11,7 +11,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and 
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 module Facter
@@ -24,7 +24,7 @@ module Facter
     include Comparable
     include Enumerable
 
-    FACTERVERSION = '1.5.9'
+    FACTERVERSION = '1.5.9.kang0'
     # = Facter
     # Functions as a hash of 'facts' you might care about about your
     # system, such as mac address, IP address, Video card, etc.
