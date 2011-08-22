cd0e69698d375a0d132327d62b9187ced9f16613
michael <michael@iconoclast.net>
2011-08-22 09:10:41 -0700

    Refactored eip facts
    Merged eip_sap.rb into eip.rb
    Collect all the version.txt variants in one loop

--- lib/facter/eip.rb
+++ lib/facter/eip.rb
@@ -16,25 +16,65 @@ require 'facter'
 
 Facter.add(:pipe_home) do
   setcode do
-    ENV['PIPE_HOME'] || ('/home/pipe' if File.directory? '/home/pipe')
+    @home_pipe = ENV['PIPE_HOME'] || ('/home/pipe' if File.directory? '/home/pipe')
   end
 end
 
-Facter.add(:eip_version) do
+# Facter.add(:eip_version) do
+#   setcode do
+#     value = nil
+#     version_file = 'version.txt'
+#     dir = Facter.value(:pipe_home) || '/home/pipe'
+#     [File.join(dir,version_file)
+#     ].each do |file|
+#         File.open(file) { |openfile| value = openfile.readlines.collect(&:chomp).join('') } if FileTest.file?(file)
+#       break if value
+#     end
+#
+#     value
+#   end
+# end
+
+VERSION_FILES = {:eip_version => 'version.txt',
+                 :eip_installer_version => 'installer/installer_version.txt',
+                 :eip_sap_version => 'opt/SAPAdapter5/version.txt', }
+
+VERSION_FILES.each do |product, file|
+  @home_pipe ||= Facter.value(:pipe_home) || '/home/pipe'
+  value = nil
+  filepath = File.join(@home_pipe, file)
+  next unless FileTest.file?(filepath)
+
+  File.open(filepath) { |openfile| value = openfile.readlines.collect(&:chomp).join('') }
+  Facter.add(product) { setcode { value } } unless value.nil?
+end
+
+Facter.add(:eip_sap_zipfile) do
   setcode do
     value = nil
-    version_file = 'version.txt'
-    dir = Facter.value(:pipe_home) || '/home/pipe'
-    [File.join(dir,version_file)
-    ].each do |file|
-        File.open(file) { |openfile| value = openfile.readlines.collect(&:chomp).join('') } if FileTest.file?(file)
-      break if value
-    end
+    @home_pipe ||= Facter.value(:pipe_home) || '/home/pipe'
+    filename_glob = 'SAPAdapter*.zip'
 
-    value
+    candidate = Dir.glob(File.join(@home_pipe, filename_glob)).sort.last
+
+    value = candidate if candidate && FileTest.file?(candidate)
   end
 end
 
+# Facter.add(:eip_sap_version) do
+#   setcode do
+#     value = nil
+#     version_file = 'opt/SAPAdapter5/version.txt'
+#     dir = Facter.value(:pipe_home) || '/home/pipe'
+#     [File.join(dir,version_file)
+#     ].each do |file|
+#         File.open(file) { |openfile| value = openfile.readlines.collect(&:chomp).join('') } if FileTest.file?(file)
+#       break if value
+#     end
+#
+#     value
+#   end
+# end
 
 ['ORACLE_BASE', 'ORACLE_HOME', 'TIBCO_HOME', 'JAVA_HOME'].each do |var|
   if ENV[var]
--- lib/facter/eip_sap.rb
+++ lib/facter/eip_sap.rb
@@ -8,37 +8,39 @@
 # Caveats:
 #
 
+# this has been merged into eip.rb
+
 ## eip_sap.rb
 ## Facts related to the SAP adapter for EnergyIP
 ##
-require 'facter'
+# require 'facter'
 
-Facter.add(:eip_sap_zipfile) do
-  setcode do
-    value = nil
-    filename_glob = 'SAPAdapter*.zip'
-    dir = Facter.value(:pipe_home) || '/home/pipe'
+# Facter.add(:eip_sap_zipfile) do
+#   setcode do
+#     value = nil
+#     filename_glob = 'SAPAdapter*.zip'
+#     dir = Facter.value(:pipe_home) || '/home/pipe'
 
-    candidate = Dir.glob(File.join(dir, filename_glob)).sort.last
+#     candidate = Dir.glob(File.join(dir, filename_glob)).sort.last
 
-    value = candidate if FileTest.file?(candidate)
-  end
-end
+#     value = candidate if FileTest.file?(candidate)
+#   end
+# end
 
-# todo: dry this
-Facter.add(:eip_sap_version) do
-  setcode do
-    value = nil
-    version_file = 'opt/SAPAdapter5/version.txt'
-    dir = Facter.value(:pipe_home) || '/home/pipe'
-    [File.join(dir,version_file)
-    ].each do |file|
-        File.open(file) { |openfile| value = openfile.readlines.collect(&:chomp).join('') } if FileTest.file?(file)
-      break if value
-    end
+# # todo: dry this
+# Facter.add(:eip_sap_version) do
+#   setcode do
+#     value = nil
+#     version_file = 'opt/SAPAdapter5/version.txt'
+#     dir = Facter.value(:pipe_home) || '/home/pipe'
+#     [File.join(dir,version_file)
+#     ].each do |file|
+#         File.open(file) { |openfile| value = openfile.readlines.collect(&:chomp).join('') } if FileTest.file?(file)
+#       break if value
+#     end
 
-    value
-  end
-end
+#     value
+#   end
+# end
 
 
